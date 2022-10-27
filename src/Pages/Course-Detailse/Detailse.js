a825787d82cdd12a36b466c071865e0ac3524252
nkbadhon <badhon7574@gmail.com>
2022-10-27 18:57:57 +0600

    Pdf downloader added

--- src/Pages/Course-Detailse/Detailse.js
+++ src/Pages/Course-Detailse/Detailse.js
@@ -5,21 +5,32 @@ import Card from 'react-bootstrap/Card';
 import { Container } from 'react-bootstrap';
 import { FaClock, FaRegFilePdf } from "react-icons/fa";
 import { Link } from "react-router-dom";
+import { useReactToPrint } from 'react-to-print';
+import { useRef } from 'react';
+
+
+
 const Detailse = () => {
     const cour = useLoaderData();
 
     console.log(cour.id);
 
+    const componentRef = useRef();
+    const handlePrint = useReactToPrint({
+        content: () => componentRef.current,
+        documentTitle: 'emp-data',
+        onAfterPrint: () => alert('Downloaded')
 
+    })
 
 
     return (
-        <Container>
+        <Container ref={componentRef} style={{ width: '100%', height: window.innerHeight }} >
             <Card>
                 <Card.Header className='d-flex justify-content-between mb-2'> <div className='text-primary'><Card.Title>{cour.name}</Card.Title></div>  <div><Card.Text className='text-primary'><FaClock></FaClock>
                     {cour.duraion}
                 </Card.Text>
-                </div><div><h4><FaRegFilePdf className='text-primary text-large'> </FaRegFilePdf></h4></div>
+                </div><div><button onClick={handlePrint} className="btn btn-outline-light"><h4><FaRegFilePdf className='text-primary text-large'> </FaRegFilePdf></h4></button></div>
                 </Card.Header>
                 <Card.Img className='course_img p-4' variant="top" src={cour.photo_url} />
                 <Card.Body>
@@ -32,7 +43,7 @@ const Detailse = () => {
                     <Link to={`/checkout/${cour.id}`}><Button variant="primary">Get Access</Button></Link>
                 </Card.Body>
             </Card>
-        </Container>
+        </Container >
     );
 };
 
