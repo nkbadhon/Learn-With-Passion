23e1fab261e6645b913cfe1447eef1b8e2c654cc
nkbadhon <badhon7574@gmail.com>
2022-10-27 16:43:02 +0600

    faq done

--- src/Pages/Faq/Faq.js
+++ src/Pages/Faq/Faq.js
@@ -5,21 +5,22 @@ const Faq = () => {
     return (
         <Container>
             <div className='text-primary'>
-                <h4><strong>Question:</strong> </h4>
-                <p> <strong>Answer:</strong> </p>
+                <h4><strong>Question: </strong> Can I refund my payment if i cancel my subscription?</h4>
+                <p> <strong>Answer: </strong> No, once you are done with your payment, there is no refund or cancellation.</p>
             </div>
-            <div>
-                <h4></h4>
-                <p></p>
+            <div className='text-primary'>
+                <h4><strong>Question: </strong>How many device can i connect at a time? </h4>
+                <p> <strong>Answer: </strong>You can connect from 3 devices at a time.  </p>
             </div>
-            <div>
-                <h4></h4>
-                <p></p>
+            <div className='text-primary'>
+                <h4><strong>Question: </strong> Do i have Lifetime access in the course?</h4>
+                <p> <strong>Answer: </strong> Yes, you have lifetime access.</p>
             </div>
-            <div>
-                <h4></h4>
-                <p></p>
+            <div className='text-primary'>
+                <h4><strong>Question: </strong> Can i download the videos from website?</h4>
+                <p> <strong>Answer: </strong> No, you only can stream.</p>
             </div>
+
         </Container>
     );
 };
