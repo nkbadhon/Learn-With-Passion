157566ec3a01bfacd8f9b7650b798502dacb9442
nkbadhon <badhon7574@gmail.com>
2022-10-27 17:31:40 +0600

    404 done

--- src/Pages/NotFound/NotFound.js
+++ src/Pages/NotFound/NotFound.js
@@ -4,8 +4,9 @@ import { FaQq } from "react-icons/fa";
 
 const NotFound = () => {
     return (
-        <Container className='text-center '>
-            <h1><FaQq></FaQq></h1>
+        <Container className='text-center'>
+            <h1 ><FaQq className='text-info'></FaQq></h1>
+            <h4>....oops! Something went <span className='text-danger'>wrong</span> . Go back to home page.</h4>
         </Container>
     );
 };
